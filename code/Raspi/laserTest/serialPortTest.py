10a9851d4a8fce833500852148235f7201c0bdc6
WilNeeley <wil.neeley@gmail.com>
2015-04-12 00:40:24 +0000

    Rasberry Pi Code

---
+++ code/Raspi/laserTest/serialPortTest.py
@@ -0,0 +1,20 @@
+import serial
+import time
+
+while (1):
+	sp = serial.Serial('/dev/ttyAMA0',19200)
+	sp.flush()
+	setup = sp.read(10)
+	print "Received {0}".format(setup)
+	sp.close()
+
+	sp = serial.Serial('/dev/ttyAMA0', int(setup))
+	sp.flush()
+	print "Serial port opened at {0:d}".format(int(setup))
+	print "Reading 10 characters..."
+	recv = sp.read(10)
+	time.sleep(1)
+	sp.flush()
+	sp.close()
+	print recv
+
---
+++ code/Raspi/sockets/socket.py
@@ -0,0 +1,46 @@
+import socket
+import os 
+import serial
+import time
+
+ser = serial.Serial("/dev/ttyMFD1",19200)
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50027              # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+
+def processData(conn):
+	global mtrCmd
+	global mtrStop
+	global ser
+
+	while 1:
+		data = conn.recv(1024)
+		if not data: break
+		print "Recvd:" + data
+		spd = int(float(data))
+		spd = -spd
+		if spd > 0 and spd < 30:
+			spd =30 
+		if spd < 0 and spd > -30:
+			spd = -30 
+
+		print "Speed: ", spd
+		if spd < 0:
+			mtrCmd[0] = 0xC1
+			spd = -spd
+		else:
+			mtrCmd[0] = 0xC2
+
+		mtrCmd[1]  = spd
+		print "Motor Command:" +  mtrCmd
+		print ser.write(mtrCmd) 
+		time.sleep(.005)
+		ser.write(mtrStop)
+	
+while 1:
+	conn, addr = s.accept()
+	processData(conn)
+ 
+conn.close()
